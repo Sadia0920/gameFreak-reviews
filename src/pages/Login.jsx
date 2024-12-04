41a224a339cf48a6474590764a1ccaa84fc9d103
Sadia0920 <sadiaafrin0920@gmail.com>
2024-12-04 15:14:54 +0600

    added a login page with form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,36 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='my-7 w-6/12 mx-auto'>
+      <div className="hero bg-base-200 rounded-xl py-5">
+  <div className="hero-content flex-col w-8/12 mx-auto">
+    <div className="text-center">
+      <h1 className="text-3xl font-bold">Login now!</h1>
+    </div>
+    <div className="card bg-base-100 w-full shadow-2xl">
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" className="input input-bordered " required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+    </div>
   )
 }
