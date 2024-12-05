bbe95c4a937880071a1b26d370956211f1307c73
Sadia0920 <sadiaafrin0920@gmail.com>
2024-12-05 19:41:52 +0600

    create a login with email and password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../provider/AuthProvider';
 
 export default function Login() {
+  const {signInUser} = useContext(AuthContext);
+
+  const handleSignInUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    const user = {email,password}
+    console.log(user);
+
+    // SignInUser
+    signInUser(email,password)
+    .then(result => {
+      console.log(result.user)
+    })
+    .catch(error => {
+      console.log(error.message)
+    })
+
+  }
   return (
     <div className='my-7 w-6/12 mx-auto'>
       <div className="hero bg-base-200 rounded-xl py-5">
@@ -9,18 +31,18 @@ export default function Login() {
       <h1 className="text-3xl font-bold text-gray-800">Login now!</h1>
     </div>
     <div className="card bg-base-100 w-full shadow-2xl">
-      <form className="card-body">
+      <form onSubmit={handleSignInUser} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered " required />
+          <input type="email" placeholder="email" name='email' className="input input-bordered " required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" placeholder="password" name='password' className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { createContext, useState } from "react"
 import { auth } from './../firebase/firebase.init';
 
@@ -14,10 +14,17 @@ export default function AuthProvider({children}) {
         return createUserWithEmailAndPassword(auth,email,password)
     }
 
+    // SignIn User
+    const signInUser = (email,password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
     const userInfo = {
         user,
         loading,
         createUser,
+        signInUser,
     }
   return (
     <AuthContext.Provider value={userInfo}>
