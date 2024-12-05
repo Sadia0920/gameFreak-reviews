4ddd7b2dbc27adde20f874d9edb1ba5e39771b9c
Sadia0920 <sadiaafrin0920@gmail.com>
2024-12-05 19:29:18 +0600

    Creating a user with email and password

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../provider/AuthProvider';
 
 export default function Register() {
+
+  const {createUser} = useContext(AuthContext);
+
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const photo = form.photo.value;
+    const password = form.password.value;
+
+    const newUser = {name,photo,email,password}
+    console.log(newUser);
+
+    // CreateUser
+    createUser(email,password)
+    .then(result => {
+      console.log(result.user)
+    })
+    .catch(error => {
+      console.log(error.message)
+    })
+
+  }
   return (
     <div className='my-7 w-6/12 mx-auto'>
       <div className="hero bg-base-200 rounded-xl py-5">
@@ -9,7 +34,7 @@ export default function Register() {
       <h1 className="text-3xl font-bold text-gray-800">Register now!</h1>
     </div>
     <div className="card bg-base-100 w-full shadow-2xl">
-      <form className="card-body">
+      <form onSubmit={handleCreateUser} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Name</span>
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,27 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createContext, useState } from "react"
+import { auth } from './../firebase/firebase.init';
+
+export const AuthContext = createContext(null)
+
+export default function AuthProvider({children}) {
+    const [user,setUser]=useState(null);
+    const [loading,setLoading]=useState(true);
+
+    // Create User
+    const createUser = (email,password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    const userInfo = {
+        user,
+        loading,
+        createUser,
+    }
+  return (
+    <AuthContext.Provider value={userInfo}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
